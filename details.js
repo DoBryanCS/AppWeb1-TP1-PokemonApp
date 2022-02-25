5c4609f1cf1d570e58785bb5aae0c1e7e0545aba
Khoa <e2072765@cmaisonneuve.qc.ca>
2022-02-25 05:41:31 +0100

    Remise tp1

--- details.js
+++ details.js
@@ -1,3 +1,102 @@
 import { pokemonapiURL } from './commun.js';
 
 console.log(pokemonapiURL);
+
+let params = (new URL(document.location)).searchParams;
+let id = params.get("pokemonId");
+
+
+async function detailspokemon() {
+    const resultat = await fetch(`${pokemonapiURL}/pokemon?pokemonId=` + id);
+    if (resultat.ok) {
+        const data = await resultat.json();
+        document.getElementById("name").innerHTML = data.name;
+        document.getElementById("hp").innerHTML = data.hp;
+        document.getElementById("attack").innerHTML = data.attack;
+        document.getElementById("defense").innerHTML = data.defense;
+        document.getElementById("height").innerHTML = data.height;
+        document.getElementById("specialattack").innerHTML = data.specialattack;
+        document.getElementById("specialdefense").innerHTML = data.specialdefense;
+        document.getElementById("speed").innerHTML = data.speed;
+        document.getElementById("weight").innerHTML = data.weight;
+        document.getElementById("imgURL").src = data.imgURL;
+        document.getElementById("cryURL").innerHTML = data.cryURL;
+        document.getElementById("habitatName").innerHTML = data.habitat.name;
+        document.getElementById("speciesName").innerHTML = data.species.name;
+        document.getElementById("evolutionName").innerHTML = data.evolution.name;
+        document.getElementById("evolutionImgURL").src = data.evolution.imgURL;
+        document.getElementById("evolutionEvolutionName").innerHTML = data.evolution.evolution.name;
+        document.getElementById("evolutionEvolutionImgURL").src = data.evolution.evolution.imgURL;
+        }
+    }
+    detailspokemon();
+
+
+    function verifConnection() {
+        if (sessionStorage.getItem("a") != null) {
+                document.getElementById("afficherDeconnexion").setAttribute("class", "button is-light is-danger");
+                document.getElementById("favorites").setAttribute("class", "navbar-item");
+        } else {  
+                document.getElementById("afficherDeconnexion").setAttribute("class", "button is-light is-danger is-hidden");   
+        }
+    }
+    
+
+    document.getElementById('afficherDeconnexion').addEventListener('click', () => {
+        sessionStorage.clear();
+        window.location.href = './index.html'
+    });
+
+    
+    document.getElementById('ajouterFavoris').addEventListener('click', async () => {
+        const bearerToken = 'Bearer ' + sessionStorage.getItem('a');
+        const resultat = await fetch(`${pokemonapiURL}/favorite?pokemonId=` + id, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            headers: { authorization: bearerToken },
+        });
+        if (resultat.ok) {
+            
+            window.location.reload();
+        }
+
+    })
+
+    document.getElementById('supprimerFavoris').addEventListener('click', async () => {
+        const bearerToken = 'Bearer ' + sessionStorage.getItem('a');
+        const resultat = await fetch(`${pokemonapiURL}/favorite?pokemonId=` + id, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'DELETE',
+            headers: { authorization: bearerToken },
+        });
+        if (resultat.ok) {
+            
+            window.location.reload();
+        }
+
+    })
+
+
+  
+async function buttonFavoris() {
+    const bearerToken = 'Bearer ' + sessionStorage.getItem('a');
+        const resultat = await fetch(`${pokemonapiURL}/favorite?pokemonId=` + id, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'GET',
+            headers: { authorization: bearerToken },
+        });
+        if (resultat.ok) {
+        const data = await resultat.json();
+        console.log(JSON.stringify(data.isFavorite))
+            if (JSON.stringify(data.isFavorite) == "true") {
+                document.getElementById("ajouterFavoris").setAttribute("class", "button is-primary is-hidden");
+                document.getElementById("supprimerFavoris").setAttribute("class", "button is-danger")
+            } else {
+                document.getElementById("ajouterFavoris").setAttribute("class", "button is-primary");
+                document.getElementById("supprimerFavoris").setAttribute("class", "button is-danger is-hidden")
+            }
+        }
+    }
+
+buttonFavoris();
+verifConnection();
--- favorites.js
+++ favorites.js
@@ -1,3 +1,64 @@
 import { pokemonapiURL } from './commun.js';
 
+
 console.log(pokemonapiURL);
+
+
+
+
+async function obtenirFavorites() {
+    const bearerToken = 'Bearer ' + sessionStorage.getItem('a');
+    const response = await fetch(`${pokemonapiURL}/favorites`, {
+    method: 'GET',
+    headers: { authorization: bearerToken },
+    });
+    // traiter la réponse
+    if (response.ok) {
+    const data = await response.json();
+    let a =''
+        for (let i = 0; i < data.length; i += 1) {
+            let poke = '';
+            for (let index = 0; index < data[i].poketypes.length; index++) {
+                if (poke.length === 0) {
+                    poke += `${data[i].poketypes[index].name}`;
+                } else {
+                    poke += `, ${data[i].poketypes[index].name}`;
+                }
+            }
+            a += `<div class="column is-3-desktop is-4-tablet is-full-mobile">
+                    <div class="card large" style="background-color:${data[i].color}">
+                        <div class="card-image" >
+                        <a href="./details.html?pokemonId=${data[i].pokemonId}">
+    
+                            <figure class="image is-square" >
+                                <img src="${data[i].imgURL}"
+                                    alt="${data[i].name}">
+                            </figure>
+                        </a>
+                        </div>
+                            <div class="card-content">
+                            <div class="media">
+                                <div class="media-content">
+                                    <p class="title is-4 is-spaced no-padding has-text-centered">${data[i].name}</p>
+                                    <p class="subtitle is-6"><b><span style='color:black'>Species : </span></b><span style='color:black'>${data[i].species.name}</span></p>
+                                    <p class="subtitle is-6"><b><span style='color:black'>Habitat : </span></b><span style='color:black'>${data[i].habitat.name}</span></p>
+                                    <p class="subtitle is-6"><b><span style='color:black'>Poketypes : </span></b><span style='color:black'>${poke}</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>`;
+            }
+            document.getElementById("pokemons").innerHTML = a;
+    } else {
+    console.error('une erreur sest produite');
+    }
+}
+
+obtenirFavorites();
+
+
+document.getElementById('afficherDeconnexion').addEventListener('click', () => {
+    sessionStorage.clear();
+    window.location.href = './index.html'
+});
